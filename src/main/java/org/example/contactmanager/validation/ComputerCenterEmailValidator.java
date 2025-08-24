e9f7a6d71f8d00051b0626d48b2656c1af261645
katterin.gramajo <katterin.gramajo@7p-group.com>
2025-08-24 14:25:27 +0200

    Add validation for email

---
+++ src/main/java/org/example/contactmanager/validation/ComputerCenterEmailValidator.java
@@ -0,0 +1,16 @@
+package org.example.contactmanager.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class ComputerCenterEmailValidator implements ConstraintValidator<ValidComputerCenterEmail, String> {
+
+    private static final String EMAIL_REGEX = "^[^\\s@]+@computercenter\\.com$";
+
+    @Override
+    public boolean isValid(String email, ConstraintValidatorContext context) {
+        if (email == null || email.isBlank()) return false;
+
+        return email.matches(EMAIL_REGEX);
+    }
+}
---
+++ src/main/java/org/example/contactmanager/validation/ValidComputerCenterEmail.java
@@ -0,0 +1,16 @@
+package org.example.contactmanager.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = { ComputerCenterEmailValidator.class })
+@Target({ ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValidComputerCenterEmail {
+    String message() default "Invalid domain email";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
