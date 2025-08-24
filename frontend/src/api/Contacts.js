5d135bb2c99eddac37a568124db488a51ed6c9de
katterin.gramajo <katterin.gramajo@7p-group.com>
2025-08-24 14:23:16 +0200

    update api to show new entity im frontend

--- frontend/src/api/Contacts.js
+++ frontend/src/api/Contacts.js
@@ -1,9 +1,16 @@
 import axios from 'axios';
 
 const API_URL = 'http://localhost:8080/api/contacts';
+const POSITIONS_API_URL = 'http://localhost:8080/api/positions';
 
 export const getContacts = () => axios.get(API_URL);
 export const getContact = (id) => axios.get(`${API_URL}/${id}`);
 export const createContact = (contact) => axios.post(API_URL, contact);
 export const updateContact = (id, contact) => axios.put(`${API_URL}/${id}`, contact);
 export const deleteContact = (id) => axios.delete(`${API_URL}/${id}`);
+
+export const getPositions = () => axios.get(POSITIONS_API_URL);
+export const getPosition = (id) => axios.get(`${POSITIONS_API_URL}/${id}`);
+export const createPosition = (position) => axios.post(POSITIONS_API_URL, position);
+export const updatePosition = (id, position) => axios.put(`${POSITIONS_API_URL}/${id}`, position);
+export const deletePosition = (id) => axios.delete(`${POSITIONS_API_URL}/${id}`);
